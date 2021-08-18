6f9ebe9d6ef6eb50a152fb658b1c850d38e387b5
yashasvi <marcosdevil123@gmail.com>
2021-08-18 12:10:07 +0530

    first commit

---
+++ Medicare/MedicareWebParent/MedicareFrontEnd/src/main/java/com/medicare/site/MedicareFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareFrontEndApplication.class, args);
+	}
+
+}
